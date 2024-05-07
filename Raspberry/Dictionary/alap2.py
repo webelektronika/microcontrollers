6e953522cc30d87167c840da4f229f924a09ba4b
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2024-05-07 16:44:00 +0200

    Create alap2.py

---
+++ Raspberry/Dictionary/alap2.py
@@ -0,0 +1,8 @@
+dict1 = {'surname':'Gipsz', 'name':'Jakab', 'year':1999, 'city':'Budapest'}
+print(dict1['surname'])
+print(dict1['year'])
+print(dict1.keys())
+print(dict1.values())
+
+del(dict1['name'])
+print(dict1)
