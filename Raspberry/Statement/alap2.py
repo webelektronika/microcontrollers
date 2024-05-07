ba140bf85777a8364b12a99538470d526c794616
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2024-05-07 17:26:44 +0200

    Create alap2.py

---
+++ Raspberry/Statement/alap2.py
@@ -0,0 +1,14 @@
+number = 0
+while number < 10:
+	number = number + 1
+	if number == 5:
+		continue
+	print(number)
+
+
+number = 0
+while number < 10:
+	number = number + 1
+	if number == 5:
+		break
+	print(number)
