2f9ec4e7053e0478dbf8b508f18e7fe711284843
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2024-05-07 16:27:56 +0200

    Create alap2.py

---
+++ Raspberry/Strings/alap2.py
@@ -0,0 +1,22 @@
+name = "Jakab"
+surname = "Gipsz"
+
+fullName = surname + name
+print(fullName)
+
+print(fullName.isalpha())
+print(fullName.isdigit())
+print(fullName.isalnum())
+print(fullName.islower())
+print(fullName.isupper())
+print(fullName.upper())
+print(fullName.lower())
+print(fullName.rstrip())
+print(fullName.lstrip())
+print(fullName.swapcase())
+
+print(fullName.capitalize())
+print(fullName.count("ka",1,40))
+print(fullName.find("ka",1,40))
+
+print(len(fullName))
