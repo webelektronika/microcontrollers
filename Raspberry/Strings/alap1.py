4ee7fe787ef4f074505de90a4ca34775b935a4aa
WebElektronika <99819339+webelektronika@users.noreply.github.com>
2024-05-07 16:17:03 +0200

    Create alap1.py

---
+++ Raspberry/Strings/alap1.py
@@ -0,0 +1,21 @@
+name = "Jakab"
+surname = "Gipsz"
+
+fullName = surname + name
+print(fullName)
+
+print(fullName[0])
+print(surname[0] + name[0])
+print(fullName[0:3])
+print(fullName[:3])
+print(fullName[3:])
+print(fullName[-1])
+print(fullName[-2])
+
+print(fullName.isalpha())
+
+print(fullName.find('p'))
+print(fullName.replace('a','O'))
+
+
+print(len(fullName))
